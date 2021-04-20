5f671d4883f7c511d2f384ecb126c20208946641
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2021-04-19 23:20:17 -0500

    pull data from the server

--- dashboard/static/dashboard/js/highcharts.js
+++ dashboard/static/dashboard/js/highcharts.js
@@ -1,120 +1,115 @@
-Highcharts.chart('piechart-container', {
-  chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: null,
-      plotShadow: false,
-      type: 'pie'
-  },
-  title: {
-      text: 'Browser market shares in January, 2018'
-  },
-  tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-  },
-  accessibility: {
-      point: {
-          valueSuffix: '%'
-      }
-  },
-  plotOptions: {
-      pie: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: {
-              enabled: false
-          },
-          showInLegend: true
-      }
-  },
-  series: [{
-      name: 'Brands',
-      colorByPoint: true,
-      data: [{
-          name: 'Chrome',
-          y: 61.41,
-          sliced: true,
-          selected: true
-      }, {
-          name: 'Internet Explorer',
-          y: 11.84
-      }, {
-          name: 'Firefox',
-          y: 10.85
-      }, {
-          name: 'Edge',
-          y: 4.67
-      }, {
-          name: 'Safari',
-          y: 4.18
-      }, {
-          name: 'Other',
-          y: 7.05
-      }]
-  }]
-});
+async function requestBarChartData() {
+  const result = await fetch('/barchart-data');
+  if (result.ok) {
+    const data = await result.json();
+    // update data
+    barChart.update(data)
+    // call it again after one second
+    setTimeout(requestBarChartData, 5000);
+  }
+}
 
-Highcharts.chart('barchart-container', {
-  chart: {
-      type: 'bar'
-  },
-  title: {
-      text: 'Historic World Population by Region'
-  },
-  subtitle: {
-      text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
-  },
-  xAxis: {
-      categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
-      title: {
-          text: null
-      }
-  },
-  yAxis: {
-      min: 0,
-      title: {
-          text: 'Population (millions)',
-          align: 'high'
-      },
-      labels: {
-          overflow: 'justify'
-      }
-  },
-  tooltip: {
-      valueSuffix: ' millions'
-  },
-  plotOptions: {
-      bar: {
-          dataLabels: {
-              enabled: true
-          }
-      }
-  },
-  legend: {
-      layout: 'vertical',
-      align: 'right',
-      verticalAlign: 'top',
-      x: -40,
-      y: 80,
-      floating: true,
-      borderWidth: 1,
-      backgroundColor:
-          Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
-      shadow: true
-  },
-  credits: {
-      enabled: false
-  },
-  series: [{
-      name: 'Year 1800',
-      data: [107, 31, 635, 203, 2]
-  }, {
-      name: 'Year 1900',
-      data: [133, 156, 947, 408, 6]
-  }, {
-      name: 'Year 2000',
-      data: [814, 841, 3714, 727, 31]
-  }, {
-      name: 'Year 2016',
-      data: [1216, 1001, 4436, 738, 40]
-  }]
+async function requestPieChartData() {
+  const result = await fetch('/piechart-data');
+  if (result.ok) {
+    const data = await result.json();
+    pieChart.update(data);
+    // call it again after one second
+    setTimeout(requestPieChartData, 5000);
+  }
+}
+
+window.addEventListener('load', function () {
+  barChart = new Highcharts.Chart({
+    chart: {
+        type: 'bar',
+        renderTo: 'barchart-container',
+        events: {
+          load: requestBarChartData
+        }
+    },
+    title: {
+        text: 'Historic World Population by Region'
+    },
+    subtitle: {
+        text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
+    },
+    xAxis: {
+        categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+        title: {
+            text: null
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Population (millions)',
+            align: 'high'
+        },
+        labels: {
+            overflow: 'justify'
+        }
+    },
+    tooltip: {
+        valueSuffix: ' millions'
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -40,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+        shadow: true
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{},{},{},{}]
+  });
+
+  pieChart = new Highcharts.Chart({
+    chart: {
+        type: 'pie',
+        renderTo: 'piechart-container',
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        events: {
+          load: requestPieChartData
+        }
+    },
+    title: {
+        text: 'Browser market shares in January, 2018'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true
+        }
+    },
+    series: [{}]
+  });
 });
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -6,8 +8,58 @@ def homepage(request):
 
 
 def barchart(request):
-    return JsonResponse({'barchart': 'data'})
+    series = [
+        {
+            'name': 'Year 1800',
+            'data': [random.randint(100,500) for i in range(4)]
+        },
+        {
+            'name': 'Year 1900',
+            'data': [random.randint(100,500) for i in range(4)]
+        },
+        {
+            'name': 'Year 2000',
+            'data': [random.randint(100,500) for i in range(4)]
+        },
+        {
+            'name': 'Year 2016',
+            'data': [random.randint(100,500) for i in range(4)]
+        }
+    ]
+
+    return JsonResponse({'series': series})
 
 
 def piechart(request):
-    return JsonResponse({'piechart': 'data'})
+    series = {
+        'name': 'Brands',
+        'colorByPoint': True,
+        'data': [
+            {
+                'name': 'Chrome',
+                'y': random.randint(1,10)
+            },
+            {
+                'name': 'Internet Explorer',
+                'y': random.randint(1,10)
+            },
+            {
+                'name': 'Firefox',
+                'y': random.randint(1,10)
+            },
+            {
+                'name': 'Edge',
+                'y': random.randint(1,10)
+            },
+            {
+                'name': 'Safari',
+                'y': random.randint(1,10)
+            },
+            {
+                'name': 'Other',
+                'y': random.randint(1,10)
+            }
+        ]
+    }
+
+    return JsonResponse({'series': series})
